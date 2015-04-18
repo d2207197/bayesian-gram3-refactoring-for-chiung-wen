4f43cf5ec21576b5d965a2a1a71390982870775c
顏孜羲 <joseph.yen@gmail.com>
2015-04-19 01:57:57 +0800

    dict sent to list sent

--- bayesian_gram3.py
+++ bayesian_gram3.py
@@ -3,7 +3,7 @@
 from __future__ import division
 import time
 import math
-from nltk.util import ngrams
+from nltk.util import ngrams as gen_ngrams
 from collections import defaultdict
 
 # Read moves from file
@@ -50,16 +50,16 @@ def sent_fourgrams(sentence):
     # sent[sentence] = list(fourgrams)
 
 
-def get_max_BPMRC(sent):
+def get_max_BPMRC(sent_length):
     # 將句子的總數依照 b佔句子整體的10% p佔句子整體的30% 依此類推
     max_BPMRC = [
         # B_max, P_mac, M....R...C
-        int(round(len(sent) * 0.1)),
-        int(round(len(sent) * 0.3)),
-        int(round(len(sent) * 0.3))]
+        int(round(sent_length * 0.1)),
+        int(round(sent_length * 0.3)),
+        int(round(sent_length * 0.3))]
     max_BPMRC += [
         len(sent) - (2 * max_BPMRC[0] + max_BPMRC[1] + max_BPMRC[2]),
-        int(round(len(sent) * 0.1))]
+        int(round(sent_length * 0.1))]
     return max_BPMRC
 
 
@@ -71,11 +71,11 @@ def bayesian(moves_prob, gram_len):
 
 def gen_new_moves(sent):
     result_moves = [[], [], [], [], []]
-    for sentence in sent:
+    for sentence, ngrams, moves in sent:
         BPMRC_TOTAL = [0.15, 0.25, 0.2, 0.25, 0.15]
         # print sent[sentence]
-        gram_len = math.log10(1 / len(sent[sentence]))
-        for gram in sent[sentence]:
+        gram_len = math.log10(1 / len(ngrams))
+        for gram in ngrams:
 
             # 若gram存在於moves={...}
             if gram in moves:
@@ -97,16 +97,53 @@ def gen_new_moves(sent):
     return result_moves
 
 
+def dual_hash(obj):
+    hash_value = hash(obj)
+    return hash_value & 0xffffff, hash_value & 0xffffff000000 >> 4 * 6
+
+
+def in_2lvl_set(two_lvl_set, obj):
+    hash1, hash2 = dual_hash(obj)
+    return hash1 in two_lvl_set and hash2 in two_lvl_set[hash1]
+
+
+def add_to_2lvl_set(two_lvl_set, obj):
+    hash1, hash2 = dual_hash(obj)
+    two_lvl_set[hash1].add(hash2)
+
+from collections import namedtuple
+
+SentData = namedtuple('SentData', ['sentence', 'ngrams', 'moves'])
+
+
+def moves_update(moves, move_indicator, max_move_sent_ngrams):
+    for gram in max_move_sent_ngrams:
+                    # gram = update_word_list[0][i]
+
+                    # 若gram存在於moves={...}
+        if gram in moves:
+            moves[gram][move_indicator] += 1
+
+        elif gram == '':
+            break
+        # 若不存在moves={...}
+        else:
+            moves_gram_data = [0.0] * 5
+            moves_gram_data[move_indicator] = 1.0
+            moves[gram] = moves_gram_data
+
+
 if __name__ == '__main__':
 
     moves = get_moves_data('moves_data_initial.txt')
 
     # Read paragraph from file
-    sent = defaultdict(list)
+    sent = []
+    sent_set = defaultdict(set)
 
     # while paragraph_count < 200:
     for paragraph_count, paragraph in enumerate(
-            open('citeseerx_descriptions.txt.100', 'r')):
+            open('citeseerx_descriptions.txt.300', 'r')):
         start_time = time.time()
 
         # 因為第100篇的paragraph被抽出來當testing 所以要跳過training
@@ -114,101 +151,63 @@ if __name__ == '__main__':
             continue
 
         for sentence in sent_tokenizer(paragraph):
-            if len(sentence) > 10 and len(sentence.split()) >= 4 and sentence not in sent:
-                sent.update(sent_fourgrams(sentence))
+            if len(sentence) > 10 and len(sentence.split()) >= 4 and not in_2lvl_set(sent_set, sentence):
 
-        max_BPMRC = get_max_BPMRC(sent)
+                add_to_2lvl_set(sent_set, sentence)
+                sent.append(SentData(sentence,
+                                     list(gen_ngrams(sentence.split(), 4)),
+                                     []))
+
+        max_BPMRC = get_max_BPMRC(len(sent))
 
         # B[],P[],M[],R[],C[]
         result_moves = gen_new_moves(sent)
         #print (sent)
-        sentences_in_this_round = sent.keys()
 
         # 所有句子當中的B，若句子A1的B為最大值，則將A1 tag 為B
-        already_found = []
+        already_found = set()
         # print "Moves is B: "
+        from operator import itemgetter
         for move_indicator in range(0, 5):
-            BPMRC = ['B', 'P', 'M', 'R', 'C']
+            BPMRC = 'BPMRC'
             # move_indicator 找出最大值的句子位置
             while max_BPMRC[move_indicator] != 0:
-                update_word_list = []
-                current_max_move_index = result_moves[
-                    move_indicator].index(max(result_moves[move_indicator]))
+                current_max_move_index = max(
+                    enumerate(result_moves[move_indicator]), key=itemgetter(1))[0]
+
+                # current_max_move_index = result_moves[
+                #     move_indicator].index(max(result_moves[move_indicator]))
                 # 若是該句已經被tag，則尋找第二高的句子為B
+
                 if current_max_move_index in already_found:
-                    result_moves[move_indicator][
-                        current_max_move_index] = -10000
-                else:
-                    already_found.append(current_max_move_index)
-                    #target_sentences = sent.items()[current_max_move_index][0]
-                    target_sentences = sentences_in_this_round[
-                        current_max_move_index]
-                    # print "target_sentences2: " + target_sentences2
-                    # print "target_sentences: " + target_sentences
-                    #print [current_max_move_index][0]
-                    # print target_sentences
-                    sent[target_sentences].append(BPMRC[move_indicator])  # Sent append
-                    # print BPMRC[move_indicator]
-                    # print sent[target_sentences]
-                    m_sent_len = len(sent[target_sentences])
+                    result_moves[move_indicator][current_max_move_index] = -10000
 
-                    # update_word_list=[]
-                    # for i in range(0,m_sent_len-2):
-                    # update_word_list.append(sent[target_sentences][i])
-                    update_word_list = []
-                    update_word_list.append(
-                        sent[target_sentences][0:m_sent_len - 2])
-                    # print "update_word_list2 = " + str(update_word_list2)
-                    max_BPMRC[move_indicator] = max_BPMRC[
-                        move_indicator] - 1
-                    # print "update_word_list= " + str(update_word_list)
-                    word_len = len(update_word_list[0])
+                already_found.add(current_max_move_index)
+                #target_sentences = sent.items()[current_max_move_index][0]
+                # target_sentences = sentences_in_this_round[
+                # current_max_move_index]
+                # print "target_sentences2: " + target_sentences2
+                # print "target_sentences: " + target_sentences
+                #print [current_max_move_index][0]
+                # print target_sentences
+                sent[current_max_move_index].moves.append(BPMRC[move_indicator])
+                # print BPMRC[move_indicator]
+                # print sent[target_sentences]
+                # m_sent_len = len(sent[current_max_move_index].ngrams)
 
-                    # 將sentences上的4-gram update到moves={...}上
-                    for i in range(0, word_len):
-                        gram = update_word_list[0][i]
+                # update_word_list=[]
+                # for i in range(0,m_sent_len-2):
+                # update_word_list.append(sent[target_sentences][i])
+                # update_word_list = []
+                # update_word_list.append(
+                # sent[current_max_move_index].ngrams)
+                # print "update_word_list2 = " + str(update_word_list2)
+                max_BPMRC[move_indicator] -= 1
+                # print "update_word_list= " + str(update_word_list)
+                # word_len = len(update_word_list[0])
 
-                        # 若gram存在於moves={...}
-                        if gram in moves:
-                            update_list = moves.get(gram)
-                            if move_indicator == 0:
-                                update_list[0] = update_list[0] + 1
-                            elif move_indicator == 1:
-                                update_list[1] = update_list[1] + 1
-                            elif move_indicator == 2:
-                                update_list[2] = update_list[2] + 1
-                            elif move_indicator == 3:
-                                update_list[3] = update_list[3] + 1
-                            elif move_indicator == 4:
-                                update_list[4] = update_list[4] + 1
-                        elif gram == '':
-                            break
-                        # 若不存在moves={...}
-                        else:
-                            if move_indicator == 0:
-                                add_list = {
-                                    gram: [1.0, 0.0, 0.0, 0.0, 0.0]}
-                                moves.update(add_list)
-
-                            elif move_indicator == 1:
-                                add_list = {
-                                    gram: [0.0, 1.0, 0.0, 0.0, 0.0]}
-                                moves.update(add_list)
-
-                            elif move_indicator == 2:
-                                add_list = {
-                                    gram: [0.0, 0.0, 1.0, 0.0, 0.0]}
-                                moves.update(add_list)
-
-                            elif move_indicator == 3:
-                                add_list = {
-                                    gram: [0.0, 0.0, 0.0, 1.0, 0.0]}
-                                moves.update(add_list)
-
-                            elif move_indicator == 4:
-                                add_list = {
-                                    gram: [0.0, 0.0, 0.0, 0.0, 1.0]}
-                                moves.update(add_list)
+                # 將sentences上的4-gram update到moves={...}上
+                moves_update(moves, move_indicator, sent[current_max_move_index].ngrams)
 
         print ("paragraph_count: " + str(paragraph_count))
         print("--- %s seconds ---" % (time.time() - start_time))
